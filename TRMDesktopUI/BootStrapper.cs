bde6efc48b382696f7f6363cf1aa8f4b388ebb3f
TedHenschen <tedhenschen@gmail.com>
2020-03-02 17:55:56 -0500

    Add WPF Project with MVVM
    Adds our WPF uiser interface project
    and configures it with Caliburn Micro for our MVVM Framework
    Also includes the simple container DI system

---
+++ TRMDesktopUI/BootStrapper.cs
@@ -0,0 +1,65 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    public class BootStrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+
+        public BootStrapper()
+        {
+            Initialize();
+        }
+
+        //Configure is ran once at the begining of the application
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+            //A singleton creates one instance of the class for the life of the application.
+            //These are kinda like a static class, we use them so we don't have to manage multiple instances of each.
+            //These should be used as little as possible, and should only be used if you can't find a better way.
+            _container
+                //Iwindowmanager is an interface
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            //using reflection gettype (reflection is slow, use sparingly).  Only being used once in this app
+            //This finds all the viewmodlels and put them in the _container adding interfaces to our viewmodel
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //This tells the system to start ShellViewModel on Startup in bootstrap
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);   
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
